93fd8bff2f0bc34ada40d8d8c3ea486ea84ce427
ahmed.iqu <jniqbal1@gmail.com>
2020-03-18 14:32:42 +0600

    Add Express.io

--- p1/server.js
+++ p1/server.js
@@ -1,8 +1,18 @@
-var express = require('express');
+var express = require('express.io');
 var app = express();
+app.http().io();
+var PORT = 7000;
 console.log('server started');
 
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
 	res.render('index.ejs');
 });
-app.listen(7000);
+
+app.io.route('ready', function(req){
+	req.io.join(req.data);
+	app.io.room(req,data).broadcast('announce', {
+		message: 'new client in the' + req.data + 'room.'
+	})
+})
+app.listen(PORT);
